2253dfdc2caf03e80b4471e96cf9ac76d5507c48
Tauqeer8651 <alamtauqeer589@gmail.com>
2024-10-06 10:47:16 +0530

    support add

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
       <h1>Private Talker</h1>
       <ChatBox />
+      <p>
+     
+     Hii This is a simple chatting application where you can talk anonymously without using any account or name.
+     <b>if you have any problem contact us or send us your feedback on  <a href="mailto:tauqeeralam108@gmail.com?subject=Support Request&body=Hello, I need help with...">Contact Support</a> </b>   
+    
+      </p>
     </div>
   );
 }
